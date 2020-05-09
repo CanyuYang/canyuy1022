63203d48f4be172043e7f7409f3a2adf9fe16aee
CanyuYang <63687718+CanyuYang@users.noreply.github.com>
2020-05-09 13:49:55 -0400

    Update HW3.py

--- HW3.py
+++ HW3.py
@@ -1,14 +1,14 @@
+import numpy as np
 import pandas as pd
 from pandas import Series, DataFrame
 
-data = {'a': [1,4,9],
-        'b': [3,8,7],
-        'c': [5,2,3],
-        'd': [9,9,4]}
-frame = pd.DataFrame(data)
+data = np.random.randint(1,10,size=(3,4))
+data
+frame = pd.DataFrame(data,columns=['d','c','b','a'])
 frame
-frame2 = DataFrame(data,columns=['a','b','c','d','E'])
+frame = frame.sort_index(axis=1)
+frame2 = DataFrame(frame,columns=['a','b','c','d','E'])
 frame2
 val = Series(['abcd', 'cabd', 'cdba'])
 frame2['E']=val
-frame2.sort_values(by='E')
+frame2
