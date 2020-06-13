f96866095c4760ef0eedec03036cdea316cb675b
CanyuYang <63687718+CanyuYang@users.noreply.github.com>
2020-06-12 23:51:51 -0400

    Add files via upload

---
+++ Project 3_flask/auth.py
@@ -0,0 +1,152 @@
+import functools
+import datetime
+import logging
+from flask import Blueprint
+from flask import flash
+from flask import g
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import session
+from flask import url_for
+from werkzeug.security import check_password_hash
+from werkzeug.security import generate_password_hash
+
+from flaskr.db import get_db
+
+from datetime import datetime
+
+bp = Blueprint("auth", __name__, url_prefix="/auth")
+log = logging.getLogger(__name__)
+
+
+
+from functools import wraps
+from time import time
+def time_spend(func):
+    @wraps(func)
+    def _time_it(*args, **kwargs):
+        start = start = datetime.now()
+        try:
+            return func(*args, **kwargs)
+        finally:
+            end_ = start = datetime.now() - start
+            print(f"Total execution time:", end_ , "ms")
+    return _time_it
+
+#def time_spend(view):
+    #@functools.wraps(view)
+    #def wrapped_view(**kwargs):
+       # return view(**kwargs)
+
+     #time = datetime.now() - start_time
+    #return time
+
+def login_required(view):
+    """View decorator that redirects anonymous users to the login page."""
+
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for("auth.login"))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    """If a user id is stored in the session, load the user object from
+    the database into ``g.user``."""
+    user_id = session.get("user_id")
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = (
+            get_db().execute("SELECT * FROM user WHERE id = ?", (user_id,)).fetchone()
+        )
+
+
+@bp.route("/register", methods=("GET", "POST"))
+def register():
+    """Register a new user.
+
+    Validates that the username is not already taken. Hashes the
+    password for security.
+    """
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        alias =  request.form["alias"]
+        db = get_db()
+        error = None
+
+        if not username:
+            error = "Username is required."
+        elif not password:
+            error = "Password is required."
+        elif (
+            db.execute("SELECT id FROM user WHERE username = ?", (username,)).fetchone()
+            is not None
+        ):
+            error = f"User {username} is already registered."
+
+        if error is None:
+            # the name is available, store it in the database and go to
+            # the login page
+            db.execute(
+                "INSERT INTO user (username, password,alias) VALUES (?, ?, ?)",
+                (username, generate_password_hash(password),alias),
+            )
+            db.commit()
+            return redirect(url_for("auth.login"))
+
+        flash(error)
+
+    return render_template("auth/register.html")
+
+
+@bp.route("/login", methods=("GET", "POST"))
+def login():
+    """Log in a registered user by adding the user id to the session."""
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        db = get_db()
+        error = None
+        user = db.execute(
+            "SELECT * FROM user WHERE username = ?", (username,)
+        ).fetchone()
+
+        if user is None:
+            error = "Incorrect username."
+        elif not check_password_hash(user["password"], password):
+            error = "Incorrect password."
+
+        if error is None:
+            # store the user id in a new session and return to the index
+            session.clear()
+            session["user_id"] = user["id"]
+            return redirect(url_for("index"))
+
+        flash(error)
+
+    return render_template("auth/login.html")
+
+
+@bp.route("/logout")
+def logout():
+    """Clear the current session, including the stored user id."""
+    session.clear()
+    return redirect(url_for("index"))
+
+bp.route('/api/v1.0/users', methods=['GET'])
+def get_users():
+    db = get_db
+    users = db.execute(
+        "SELECT id, username  FROM user"
+        "order by id "
+    ).fetchall()
+    return jsonify({'tasks': users})
